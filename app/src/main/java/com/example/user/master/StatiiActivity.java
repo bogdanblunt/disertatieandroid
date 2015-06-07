f9ae6d0199f3f71f5a2ee964444b96997e254e61
musatbogdanionut <musatbogdanionut@gmail.com>
2015-06-08 01:14:06 +0300

    Fixed imports and Statii panel work.

--- app/src/main/java/com/example/user/master/StatiiActivity.java
+++ app/src/main/java/com/example/user/master/StatiiActivity.java
@@ -1,13 +1,17 @@
 package com.example.user.master;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.util.TypedValue;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.example.user.master.dbUtils.DisertatieDatabaseHelper;
@@ -26,19 +30,45 @@ public class StatiiActivity extends ActionBarActivity {
         AutoCompleteTextView autoCompleteStatii = (AutoCompleteTextView)findViewById(R.id.autocompleteStatii);
         final DisertatieDatabaseHelper disertatieDatabaseHelper = new DisertatieDatabaseHelper(this);
         List<String> statiiNumarList = disertatieDatabaseHelper.getNumeStatiiList();
-        ArrayAdapter<String> statiiArrayAdapter = new ArrayAdapter<String>(this, R.layout.support_simple_spinner_dropdown_item, statiiNumarList);
+        ArrayAdapter<String> statiiArrayAdapter = new ArrayAdapter<>(this, R.layout.support_simple_spinner_dropdown_item, statiiNumarList);
         autoCompleteStatii.setAdapter(statiiArrayAdapter);
         autoCompleteStatii.setOnItemClickListener(new AdapterView.OnItemClickListener() {
 
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                HashMap<String, List<String>> liniiForStatie = disertatieDatabaseHelper.getLiniiAndStatiiByStatie(parent.getItemAtPosition(position).toString());//getLiniiByStatie(parent.getItemAtPosition(position).toString());
-
-
+                HashMap<String, List<String>> liniiForStatie = disertatieDatabaseHelper.getLiniiAndStatiiByStatie(parent.getItemAtPosition(position).toString());
+                LinearLayout container = (LinearLayout) findViewById(R.id.statiiPanelContainer);
+                container.removeAllViews();
+                int counter = 0;
+                LinearLayout rowContainer = null;
                 for (String linie : liniiForStatie.keySet()) {
+
+                    if(counter%3==0){
+                        rowContainer = new LinearLayout(getBaseContext());
+                        rowContainer.setOrientation(LinearLayout.HORIZONTAL);
+                        rowContainer.setWeightSum(3);
+                        container.addView(rowContainer);
+                    }
+
+                    LinearLayout statiiContainer = new LinearLayout(getBaseContext());
+                    statiiContainer.setOrientation(LinearLayout.VERTICAL);
+                    rowContainer.addView(statiiContainer, ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+                    statiiContainer.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT, 1f));
                     List<String> statiiList = liniiForStatie.get(linie);
-                    TextView liniiByStatiiTv = (TextView)findViewById(R.id.liniiByStatiiResult);
-                    liniiByStatiiTv.setText(statiiList.toString());
+                    TextView tvLinieNumber = new TextView(getBaseContext());
+                    tvLinieNumber.setText(linie);
+                    tvLinieNumber.setTextSize(TypedValue.COMPLEX_UNIT_SP, 22);
+                    tvLinieNumber.setTextColor(Color.BLACK);
+                    statiiContainer.addView(tvLinieNumber, ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+
+                    for(String statie : statiiList){
+                        TextView tvLinieWithStatii = new TextView(getBaseContext());
+                        tvLinieWithStatii.setText(statie);
+                        tvLinieWithStatii.setTextSize(TypedValue.COMPLEX_UNIT_SP, 18);
+                        tvLinieWithStatii.setTextColor(Color.BLACK);
+                        statiiContainer.addView(tvLinieWithStatii, ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+                    }
+                    counter++;
                 }
             }
         });
@@ -66,6 +96,5 @@ public class StatiiActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
-
 
 }
